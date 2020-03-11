69e005f1e6792e0e0e9ffeabd018e771124e3bdd
Miguel Martinez Costa <56074037+Miguelmc5@users.noreply.github.com>
2020-03-11 09:50:31 +0100

    Add files via upload

---
+++ src/PersonUnica.kt
@@ -0,0 +1,11 @@
+object PersonUnica {
+
+    var nombre: String = "Pepe"
+    var edad: Int = 23
+    var pasos: Int =0
+    fun avnzar(){
+        pasos = pasos +1
+    }
+
+
+}
---
+++ src/app.kt
@@ -0,0 +1,27 @@
+fun main(){
+
+
+    var pepe :Persona=Persona("pepe","Garcia", 90)
+    println(pepe.toString())
+    println(Persona.toString())
+    var manuel :Persona=Persona("manuel","Lopez", 12)
+    println(manuel.toString())
+    println(Persona.toString())
+    var jose :Persona=Persona("jose","bb", 24)
+    println(jose.toString())
+    println(Persona.toString())
+    var aa :Persona=Persona("aa","cc", 44)
+    println(aa.toString())
+    println(Persona.toString())
+    var dd :Persona=Persona("dd","ee", 2)
+    println(dd.toString())
+    println(Persona.toString())
+
+    println(Persona.toString())
+
+    println("****"+PersonUnica.nombre+"****")
+    PersonUnica.avnzar()
+    PersonUnica.avnzar()
+    println(PersonUnica.pasos)
+
+}
